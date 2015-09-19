bc2ec9aa4aa06111245768f59f2ab336cbb0ce5b
Noah Lange <noahrlange@gmail.com>
2015-09-19 13:01:49 -0500

    Fixed misunderstanding of sentence-tokenizer.

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ module.exports = (text, opts) => {
       tokenizer.setEntry(text);
 
   var sentences = tokenizer.getSentences();
-  var suggestions = sentences.map(sentence => {
+  var suggestions = sentences.map((sentence, i) => {
     
     // Calculate the FK score of each sentence.
     var fk = fleschKincaid({
       sentence: 1,
-      word: tokenizer.getTokens().length,
+      word: tokenizer.getTokens(i).length,
       syllable: syllable(sentence)
     });
-    
+        
     var index = text.indexOf(sentence);
     var offset = sentence.length;
     
     // Return a suggestion if it's over the specified FK level.    
     return fk > opts.level && index !== -1? {
-      reason: `has a Flesch-Kincaid readibility score is ${ fk.toFixed(3) }`,
+      reason: `has a Flesch-Kincaid grade level of ${ fk.toFixed(1) }`,
       index: index,
       offset: offset
     } : null;
@@ -38,4 +38,3 @@ module.exports = (text, opts) => {
   
   return suggestions;
 };
-
