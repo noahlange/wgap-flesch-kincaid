294c6856158d0b79e98eedacfed8bff1fefc8d8e
Noah Lange <noahrlange@gmail.com>
2015-09-19 09:04:30 -0500

    Swapped out tokenizer.

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 var fleschKincaid = require('flesch-kincaid');
 var syllable = require('syllable');
-var tokenize = require('./node_modules/rousseau/lib/en/english').sentences();
+var Tokenizer = require('sentence-tokenizer');
 
 module.exports = (text, opts) => {
-  
-  // From Rousseau
-  var WORD_BOUNDARY_CHARS = '\t\r\n\u00A0 !\"#$%&()*+,\-.\\/:;<=>?@\[\\\]^_`{|}~';
-  var WORD_BOUNDARY_REGEX = new RegExp('[' + WORD_BOUNDARY_CHARS + ']');
-  var SPLIT_REGEX = new RegExp(
-      '([^' + WORD_BOUNDARY_CHARS + ']+)');
   
   opts = opts || { level: 12 };
   
   // Cut text into sentences
-  var sentences = tokenize(text);
+  
+  var tokenizer = new Tokenizer('prosely');
+      tokenizer.setEntry(text);
+
+  var sentences = tokenizer.getSentences();
   var suggestions = sentences.map(sentence => {
     
     // Calculate the FK score of each sentence.
     var fk = fleschKincaid({
       sentence: 1,
-      word: sentence.value.split(WORD_BOUNDARY_REGEX).length,
+      word: tokenizer.getTokens().length,
       syllable: syllable(sentence.value)
     });
+    
+    var index = text.indexOf(sentence);
+    var offset = sentence.length;
     
     // Return a suggestion if it's over the specified FK level.    
     return fk > opts.level ? {
       reason: `has a Flesch-Kincaid readibility score is ${ fk.toFixed(3) }`,
-      index: sentence.index,
-      offset: sentence.index + sentence.offset
+      index: index,
+      offset: offset
     } : null;
     
   // Filter out okay sentences.
